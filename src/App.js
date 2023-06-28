8c6f4ba3b3d6cc4874394eab5d32fc8478b95ec3
Siraj Samsudeen <mailsiraj@gmail.com>
2023-06-28 11:42:38 +0530

    2 - fetch categories from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const categories = [
-    'electronics',
-    'jewelery',
-    "men's clothing",
-    "women's clothing",
-  ];
+  const [categories, setCategories] = useState([]);
+
+  useEffect(fetchCategories, []);
+
+  function fetchCategories() {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }
 
   return (
     <div className="App">
@@ -17,7 +20,12 @@ export default function App() {
 }
 
 function CategoryList({ categories }) {
+  // TODO first category is NOT highlighted on first render
+  // because setSelectedCategory is not yet executed
+  // I know why this is happening - what is the right way to handle this?
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  console.log('CategoryList => ', categories);
+  console.log('selected category ', selectedCategory);
 
   return (
     <div className="category-list">
