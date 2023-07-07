ec5542a859257578bb4c2a9173c7bca56519874a
Siraj Samsudeen <mailsiraj@gmail.com>
2023-07-07 08:39:25 +0530

    Fix bug - class instead of className in Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,7 @@ export function Product({ image, title, price }) {
   return (
     <div className="product">
       <img src={image} alt={title} />
-      <p class="product-title">{title}</p>
+      <p className="product-title">{title}</p>
       <p>Rs. {price}</p>
     </div>
   );
